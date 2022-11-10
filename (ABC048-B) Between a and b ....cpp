10adb4e8ead05a45bf999cf2d92a4059579b1bea
NEETNETNET <112066731+NEETNETNET@users.noreply.github.com>
2022-11-10 10:11:09 +0700

    C++

--- (ABC048-B) Between a and b ....cpp
+++ (ABC048-B) Between a and b ....cpp
@@ -1,3 +1,4 @@
+// cach 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,3 +9,17 @@ int main() {
   else cout<<(b/x-a/x)+1;
 
 }
+
+// cach 2 sai test 8 
+/* #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long a,b,x;
+  cin >> a >> b >> x;
+  double k=a;
+  k/=x;
+  long long l=ceil(k);
+  cout << (b/x)-l+1;
+  
+} */
